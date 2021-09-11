7c64d500beacead92fd58096283a74bd678c757a
ferrazandre <55859517+ferrazandre@users.noreply.github.com>
2021-09-11 10:15:24 -0300

    Create person service

--- src/main/java/curso/personapi/services/PersonService.java
+++ src/main/java/curso/personapi/services/PersonService.java
@@ -1,18 +1,62 @@
 package curso.personapi.services;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import curso.personapi.domain.dto.PersonDTO;
+import curso.personapi.domain.dto.PhoneDTO;
 import curso.personapi.domain.entity.Person;
-import curso.personapi.repositorys.PersonRepository;
+import curso.personapi.domain.entity.Phone;
+import curso.personapi.repositories.PersonRepository;
+import curso.personapi.repositories.PhoneRepository;
 
 @Service
 public class PersonService {
 
 	@Autowired
 	private PersonRepository repository;
+	@Autowired
+	private PhoneRepository phoneRepository;
 
-	public Person createPerson() {
-		return null;
+	@Transactional(readOnly = true)
+	public List<PersonDTO> findAll() {
+		List<Person> list = repository.findAll();
+		return list.stream().map(person -> new PersonDTO(person)).collect(Collectors.toList());
 	}
+
+	@Transactional
+	public PersonDTO createPerson(PersonDTO dto) {
+		Person person = new Person(null, dto.getFirstName(), dto.getLastName(), dto.getCpf(), dto.getBirthDate());
+		for (PhoneDTO phoneDTO : dto.getPhones()) {
+			Phone phone = phoneRepository.getById(phoneDTO.getId());
+			person.getPhones().add(phone);
+		}
+		repository.save(person);
+		return new PersonDTO(person);
+	}
+
+	@Transactional
+	public PersonDTO findByCpf(String cpf) {
+		Optional<Person> person = repository.findByCpf(cpf);
+		if (person.isEmpty()) {
+			throw new IllegalArgumentException("CPF inválido");
+		}
+		return new PersonDTO(person.get());
+	}
+
+	@Transactional
+	public PersonDTO removePerson(Long id) {
+		Optional<Person> person = repository.findById(id);
+		if (person.isEmpty()) {
+			throw new IllegalArgumentException("ID inválido");
+		}
+		repository.delete(person.get());
+		return new PersonDTO(person.get());
+	}
+
 }
